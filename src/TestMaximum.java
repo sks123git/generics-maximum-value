5e24b21fe198ab8206fdec471961575ae66db4fd
subzero <shubham8111994@gmail.com>
2023-02-18 23:21:40 +0530

    Initial commit

---
+++ src/TestMaximum.java
@@ -0,0 +1,6 @@
+//Program to test maximum among 3 values using generics
+public class TestMaximum {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
